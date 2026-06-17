630c304301783093e961dcccdc596f98476c40c8
NikhilTeja2000 <chntus2000@gmail.com>
2026-06-17 11:01:55 -0500

    enhance example script by adding data type prints and filtering for orders over 100000

--- day1/example1.py
+++ day1/example1.py
@@ -12,11 +12,18 @@ df = spark.read.csv("data/tpch/customer.tbl", sep="|", inferSchema=True, header=
 orders = spark.read.csv("data/tpch/orders.tbl", sep="|", inferSchema=True, header=False)
 df.show(5)
 df.printSchema()
+print(type(df))
 
 orders.show(5,truncate=False)
 orders.printSchema()
 print("count of customer table: ", df.count())
 print("count of orders table: ", orders.count())
+orders.select("_c0", "_c1", "_c3").filter(orders["_c3"] > 100000).show(5)
+
 
 print("number of partitions in customer table: ", df.rdd.getNumPartitions())
 print("number of partitions in orders table: ", orders.rdd.getNumPartitions())
+
+pandas_df=df.limit(1000).toPandas()
+print(type(pandas_df))
+print(pandas_df.head())
