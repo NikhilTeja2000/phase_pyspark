d345c88e202296f5abcc8f96a5dcc91dba69cd19
NikhilTeja2000 <chntus2000@gmail.com>
2026-06-16 22:13:45 -0500

    add initial project files including .gitignore, example script, and requirements

--- example.py
+++ example.py
@@ -1,2 +1,9 @@
-for a in range(10):
-    print(a)
+from pyspark.sql import SparkSession
+
+spark =(SparkSession.builder
+        .appName("learning-pyspark")
+        .master("local[*]")
+        .config("spark.driver.memory", "2g")
+        .getOrCreate()
+        )
+print("spark version: ", spark.version)
